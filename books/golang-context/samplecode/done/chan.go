f6cebd548077cd67c24333b3d68ef166722fd949
H.Saki <44340370+saki-engineering@users.noreply.github.com>
2021-08-28 17:24:18 +0900

    context本3章のコードを追加

---
+++ books/golang-context/samplecode/done/chan.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func generator(done chan struct{}, num int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer wg.Done()
+
+	LOOP:
+		for {
+			select {
+			case <-done:
+				break LOOP
+			case out <- num:
+			}
+		}
+
+		close(out)
+		fmt.Println("generator closed")
+	}()
+	return out
+}
+
+func main() {
+	done := make(chan struct{})
+	gen := generator(done, 1)
+
+	wg.Add(1)
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(<-gen)
+	}
+	close(done)
+
+	wg.Wait()
+}
